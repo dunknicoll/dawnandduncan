8714d08ae5a20b25b0d2a24fb13525d8945bf72c
Dunks <duncan.nicoll@gmail.com>
2018-05-23 02:27:24 +0100

    a few tweaks

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,12 @@
 const nav = document.querySelector('#navigation')
 const menuButton = document.querySelector('#menuButton')
 const body = document.body
+const html = document.html
 
 menuButton.addEventListener('click', e => {
     e.preventDefault()
     nav.classList.toggle('hidden')
     menuButton.classList.toggle('is-active')
     body.classList.toggle('stop-scrolling')
+    html.classList.toggle('stop-scrolling')
 })
