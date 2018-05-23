73177d7499856801c7d2d5419653ee7b86d99e66
Dunks <duncan.nicoll@gmail.com>
2018-05-23 01:46:13 +0100

    stop scrolling

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 const nav = document.querySelector('#navigation')
 const menuButton = document.querySelector('#menuButton')
+const body = document.body
 
 menuButton.addEventListener('click', e => {
     e.preventDefault()
     nav.classList.toggle('hidden')
     menuButton.classList.toggle('is-active')
+    body.classList.toggle('stop-scrolling')
 })
