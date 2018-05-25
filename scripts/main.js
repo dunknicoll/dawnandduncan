af7e31c1ad35b750116b3ea66f9d2865befe79c4
Dunks <duncan.nicoll@gmail.com>
2018-05-25 21:53:57 +0100

    sorting style

--- scripts/main.js
+++ scripts/main.js
@@ -15,8 +15,10 @@ function removeFooter(limitCheck, limitCount) {
 document.addEventListener('DOMContentLoaded', function (o) {
     var nav = document.querySelector('#navigation');
     var menuButton = document.querySelector('#menuButton');
+    var header = document.querySelector('header');
     var body = document.body;
-    var html = document.html;
+    var html = document.body.parentElement;
+    var navItems = document.querySelector('nav > a');
 
     var limitCheck = 20;
     var limitCount = 0;
@@ -25,14 +27,21 @@ document.addEventListener('DOMContentLoaded', function (o) {
 
     menuButton.addEventListener('click', function (e) {
         e.preventDefault();
+        header.classList.toggle('is-active');
         nav.classList.toggle('hidden');
         menuButton.classList.toggle('is-active');
         body.classList.toggle('stop-scrolling');
         html.classList.toggle('stop-scrolling');
 
         if (menuButton.classList.contains('is-active')) {
+            navItems.addEventListener('click', function (e) {
+                menuButton.click();
+            });
             document.ontouchmove = function (e) { e.preventDefault(); }
         } else {
+            navItems.removeEventListener('click', function (e) {
+                menuButton.click();
+            });
             document.ontouchmove = function (e) { return true; }
         }
     }.bind(this))
